b2a6cbfa3ec5a989c61fff35873dd6caf2dabe6d
Gabriel Revells <gcrevell@mtu.edu>
2019-05-21 13:54:36 -0400

    Initial Commit

---
+++ Data Structures/Data_Structures.h
@@ -0,0 +1,19 @@
+//
+//  Data_Structures.h
+//  Data Structures
+//
+//  Created by Gabriel Revells on 5/21/19.
+//  Copyright © 2019 Gabriel Revells. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Data_Structures.
+FOUNDATION_EXPORT double Data_StructuresVersionNumber;
+
+//! Project version string for Data_Structures.
+FOUNDATION_EXPORT const unsigned char Data_StructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data_Structures/PublicHeader.h>
+
+
